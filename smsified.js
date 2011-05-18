2f270bc6547b373bef64be06acfd3399f1e8fd9d
gradus <kriswindham@gmail.com>
2011-05-18 18:36:04 -0400

    initial commit

--- smsified.js
+++ smsified.js
@@ -1,5 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (node-sms) is running..');
-}).listen(10085);
+var sys = require('sys');
+var express = require('express');
+var app = express.createServer();
+var smsified = require('smsified');
+
+app.configure(function(){
+    app.use(express.bodyParser());
+});
+
+app.post('/', function(req, res){
+
+	var callerID = req.body['inboundSMSMessageNotification']['inboundSMSMessage']['senderAddress'];
+	var message = req.body['inboundSMSMessageNotification']['inboundSMSMessage']['message'];
+	var my_number = '8033603069';
+	var complete_msg = "callerID: " + callerID + " said: " + message;
+
+	var sms = new SMSified('gradus', 'bratya147');
+	var options = {senderAddress: '8436363155', address: my_number, message: complete_msg};
+
+	sms.sendMessage(options, function(result) {
+		sys.puts(sys.inspect(result));
+	});
+
+	res.end();
+});
+
+app.listen(10085);
